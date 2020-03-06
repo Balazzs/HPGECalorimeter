790fca3d94083cc9f6825741f66a4078ca4affda
Balazzs <bogye.balazs@gmail.com>
2020-03-06 01:21:24 +0100

    Complex detector geometry
    closes #3

--- source/src/B4DetectorConstruction.cc
+++ source/src/B4DetectorConstruction.cc
@@ -44,6 +44,7 @@
 #include "G4PhysicalVolumeStore.hh"
 #include "G4LogicalVolumeStore.hh"
 #include "G4SolidStore.hh"
+#include "G4SubtractionSolid.hh"
 
 #include "G4VisAttributes.hh"
 #include "G4Colour.hh"
@@ -89,7 +90,9 @@ void B4DetectorConstruction::DefineMaterials()
   // Lead material defined using NIST Manager
   auto nistManager = G4NistManager::Instance();
   nistManager->FindOrBuildMaterial("G4_Ge");
+  nistManager->FindOrBuildMaterial("G4_Al");
   nistManager->FindOrBuildMaterial("G4_AIR");
+  nistManager->FindOrBuildMaterial("G4_TEFLON");
   
   // Liquid argon material
   G4double a;  // mass of a mole;
@@ -98,7 +101,7 @@ void B4DetectorConstruction::DefineMaterials()
   // Vacuum
   new G4Material("Galactic", z=1., a=1.01*g/mole,density= universe_mean_density,
                   kStateGas, 2.73*kelvin, 3.e-18*pascal);
-
+  
   // Print materials
   G4cout << *(G4Material::GetMaterialTable()) << G4endl;
 }
@@ -106,29 +109,81 @@ void B4DetectorConstruction::DefineMaterials()
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4VPhysicalVolume* B4DetectorConstruction::DefineVolumes()
-{
+{  
   // Geometry parameters
-  G4double calorSize  = 7.*cm;
+  G4double crystalDiameter            = 51. *mm;
+  G4double crystalLength              = 42. *mm;
+  G4double crystalHoleDiameter        = 7.5 *mm;
+  G4double crystalHoleDepth           = 37.5*mm;
   
-  auto worldSize = 1.2 * calorSize;
+  G4double endCapThickness            = 1.5 *mm;
+  G4double endCapOuterDiameter        = 7.6 *cm;
+  G4double endCapInnerDiameter        = endCapOuterDiameter - endCapThickness;
+  G4double endCapLength               = 7.  *cm;//TODO...
+  
+  G4double crystalHolderThickness     = 0.76 *mm;
+  G4double crystalHolderInnerDiameter = crystalDiameter; //TODO actually there is a 0.5 mm boron layer on the outside of the crystal
+  G4double crystalHolderOuterDiameter = crystalHolderInnerDiameter + crystalHolderThickness;
+  G4double crystalHolderExtraLength   = 8. * mm;//TODO random guess
+  G4double crystalHolderLength        = crystalLength + crystalHolderExtraLength;
+  G4double crystalHolderEndThickness  = 3.2 *mm;
+  
+  G4double protrusionLength           = 8. * mm;//TODO random guess
+  G4double protrusionThickness        = crystalHolderEndThickness;
+  G4double protrusionOuterDiameter    = 30 *mm;//TODO random guess
+  G4double protrusionInnerDiameter    = protrusionOuterDiameter - protrusionThickness;
+  
+  G4double teflonDiameter             = protrusionInnerDiameter;
+  G4double teflonLength               = protrusionLength - protrusionThickness;//...
+  
+  // Positions - endcap starts at 0
+  
+  G4double windowDistance          = 5. *mm;
+  
+  G4double calorimeterShift        =  endCapLength / 2;
+  G4double START                   = -endCapLength / 2;
+  
+  G4double endCapShift             = START + endCapLength / 2;
+  G4double endCapDisk1Shift        = START + endCapThickness / 2;
+  G4double endCapDisk2Shift        = START + endCapLength - endCapThickness / 2;
+  
+  G4double crystalShift            = START + windowDistance + crystalLength / 2;
+  G4double crystalHoleRelShift     = crystalLength / 2 - crystalHoleDepth / 2;
+  
+  G4double crystalHolderShift      = START + windowDistance + crystalHolderLength / 2;
+  //G4double crystalHolderDisk1Shift = START + windowDistance + crystalHolderThickness / 2;//What is on the front?
+  G4double crystalHolderDisk2Shift = START + windowDistance + crystalHolderLength - crystalHolderThickness / 2;
+  
+  G4double protrusionShift         = START + windowDistance + crystalHolderLength + protrusionLength / 2;
+  G4double protrusionDiskShift     = START + windowDistance + crystalHolderLength + protrusionLength - protrusionThickness / 2;
+  G4double teflonShift             = START + windowDistance + crystalHolderLength + teflonLength / 2;
+  
+  
+  //
+  G4double calorSize = endCapLength * 1.1;
+  
+  auto worldSize = 1.1 * calorSize;
   auto ZSpace    = 1.0 *m;
   
   // Get materials
-  auto defaultMaterial = G4Material::GetMaterial("G4_AIR");
+  auto defaultMaterial  = G4Material::GetMaterial("G4_AIR");
   auto absorberMaterial = G4Material::GetMaterial("G4_Ge");
+  auto vacuum           = G4Material::GetMaterial("Galactic");
+  auto aluminium        = G4Material::GetMaterial("G4_Al");
+  auto teflon           = G4Material::GetMaterial("G4_TEFLON");
   
-  if ( ! defaultMaterial || ! absorberMaterial ) {
+  if ( ! defaultMaterial || ! absorberMaterial || !vacuum || !aluminium ) {
     G4ExceptionDescription msg;
     msg << "Cannot retrieve materials already defined."; 
     G4Exception("B4DetectorConstruction::DefineVolumes()",
       "MyCode0001", FatalException, msg);
   }
-   
-  //     
+  
+  //
   // World
   //
   
-  auto worldS 
+  auto worldS
     = new G4Box("World",           // its name
                  worldSize/2, worldSize/2, worldSize/2 + ZSpace); // its size
   
@@ -137,57 +192,128 @@ G4VPhysicalVolume* B4DetectorConstruction::DefineVolumes()
                  worldS,           // its solid
                  defaultMaterial,  // its material
                  "World");         // its name
-                                   
+  
   auto worldPV
     = new G4PVPlacement(
                  0,                // no rotation
                  G4ThreeVector(),  // at (0,0,0)
-                 worldLV,          // its logical volume                         
+                 worldLV,          // its logical volume
                  "World",          // its name
                  0,                // its mother  volume
                  false,            // no boolean operation
                  0,                // copy number
                  fCheckOverlaps);  // checking overlaps 
   
-  //                               
+  //
   // Calorimeter
-  //  
-  auto calorimeterS
-  //  = new G4Box("Calorimeter",     // its name
-  //               calorSize/2, calorSize/2, calorSize/2); // its size
-    = new G4Tubs ("Calorimeter", 0, calorSize/2, calorSize/2, 0, 2*M_PI);
+  //
   
-  auto calorLV
-    = new G4LogicalVolume(
-                 calorimeterS,     // its solid
-                 absorberMaterial,  // its material
-                 "Calorimeter");   // its name
+  auto calorimeterSolid          = new G4Box ("Calorimeter", calorSize/2, calorSize/2, calorSize/2);
+  auto calorimeterLogicalVolume  = new G4LogicalVolume (calorimeterSolid, defaultMaterial, "Calorimeter");
+  auto calorimeterPhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, calorimeterShift),
+                                                      calorimeterLogicalVolume,
+                                                      "Calorimeter",
+                                                      worldLV,
+                                                      false, 0, fCheckOverlaps);
   
-  fAbsorberPV = new G4PVPlacement(
-                 0,                // no rotation
-                 G4ThreeVector(0, 0, calorSize/2),  // position
-                 calorLV,          // its logical volume                         
-                 "Calorimeter",    // its name
-                 worldLV,          // its mother  volume
-                 false,            // no boolean operation
-                 0,                // copy number
-                 fCheckOverlaps);  // checking overlaps 
+  //------------------------
+  //--------Endcap----------
+  auto endCapSolid          = new G4Tubs ("EndCap", endCapInnerDiameter/2, endCapOuterDiameter/2, endCapLength/2, 0, 2*M_PI);
+  auto endCapLogicalVolume  = new G4LogicalVolume (endCapSolid, aluminium, "EndCap");
+  auto endCapPhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, endCapShift),
+                                                 endCapLogicalVolume,
+                                                 "EndCap",
+                                                 calorimeterLogicalVolume,
+                                                 false, 0, fCheckOverlaps);
   
-  G4cout
-    << G4endl 
-    << "------------------------------------------------------------" << G4endl
-    << "---> The calorimeter is a " << calorSize << " mm cylinder"    << G4endl
-    << "------------------------------------------------------------" << G4endl;
+  auto endCapDisk1Solid          = new G4Tubs ("EndCapDisk1", 0, endCapOuterDiameter/2 - endCapThickness/2, endCapThickness/2, 0, 2*M_PI);
+  auto endCapDisk1LogicalVolume  = new G4LogicalVolume (endCapDisk1Solid, aluminium, "EndCapDisk1");
+  auto endCapDisk1PhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, endCapDisk1Shift),
+                                                      endCapDisk1LogicalVolume,
+                                                      "EndCapDisk1",
+                                                      calorimeterLogicalVolume,
+                                                      false, 0, fCheckOverlaps);
   
-  //                                        
+  auto endCapDisk2Solid          = new G4Tubs ("EndCapDisk2", 0, endCapOuterDiameter/2 - endCapThickness/2, endCapThickness/2, 0, 2*M_PI);
+  auto endCapDisk2LogicalVolume  = new G4LogicalVolume (endCapDisk2Solid, aluminium, "EndCapDisk2");
+  auto endCapDisk2PhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, endCapDisk2Shift),
+                                                      endCapDisk2LogicalVolume,
+                                                      "EndCapDisk2",
+                                                      calorimeterLogicalVolume,
+                                                      false, 0, fCheckOverlaps);
+  
+  //------------------------
+  //--------Crystal---------
+  auto crystalCylinderSolid = new G4Tubs ("CrystalBase", 0, crystalDiameter/2,     crystalLength/2,    0, 2*M_PI);
+  auto crystalHoleSolid     = new G4Tubs ("CrystalHole", 0, crystalHoleDiameter/2, crystalHoleDepth/2, 0, 2*M_PI);
+  auto crystalSolid         = new G4SubtractionSolid ("Crystal", crystalCylinderSolid, crystalHoleSolid, 0, G4ThreeVector(0, 0, crystalHoleRelShift));
+  
+  auto crystalLogicalVolume  = new G4LogicalVolume (crystalSolid, absorberMaterial, "Crystal");
+  auto crystalPhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, crystalShift),//
+                                                 crystalLogicalVolume,
+                                                 "Crystal",
+                                                 calorimeterLogicalVolume,
+                                                 false, 0, fCheckOverlaps);
+  
+  //------------------------
+  //----Crystal holder------
+  auto crystalHolderSolid          = new G4Tubs ("CrystalHolder", crystalHolderInnerDiameter/2, crystalHolderOuterDiameter/2, crystalHolderLength/2, 0, 2*M_PI);
+  auto crystalHolderLogicalVolume  = new G4LogicalVolume (crystalHolderSolid, aluminium, "CrystalHolder");
+  auto crystalHolderPhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, crystalHolderShift),
+                                                        crystalHolderLogicalVolume,
+                                                        "CrystalHolder",
+                                                        calorimeterLogicalVolume,
+                                                        false, 0, fCheckOverlaps);
+  
+  /*auto crystalHolderDisk1Solid          = new G4Tubs ("CrystalHolderDisk1", 0, crystalHolderOuterDiameter/2 - crystalHolderThickness/2, crystalHolderThickness/2-1*nm, 0, 2*M_PI);
+  auto crystalHolderDisk1LogicalVolume  = new G4LogicalVolume (crystalHolderDisk1Solid, aluminium, "CrystalHolderDisk1");
+  auto crystalHolderDisk1PhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, crystalHolderDisk1Shift),
+                                                             crystalHolderDisk1LogicalVolume,
+                                                             "CrystalHolderDisk1",
+                                                             calorimeterLogicalVolume,
+                                                             false, 0, fCheckOverlaps);*/
+  
+  auto crystalHolderDisk2Solid          = new G4Tubs ("CrystalHolderDisk2", teflonDiameter/2, crystalHolderOuterDiameter/2 - crystalHolderThickness/2, crystalHolderThickness/2, 0, 2*M_PI);
+  auto crystalHolderDisk2LogicalVolume  = new G4LogicalVolume (crystalHolderDisk2Solid, aluminium, "CrystalHolderDisk2");
+  auto crystalHolderDisk2PhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, crystalHolderDisk2Shift),
+                                                             crystalHolderDisk2LogicalVolume,
+                                                             "CrystalHolderDisk2",
+                                                             calorimeterLogicalVolume,
+                                                             false, 0, fCheckOverlaps);
+  
+  //------------------------
+  //-------protrusion-------
+  auto protrusionSolid          = new G4Tubs ("CrystalHolderProtrusion", protrusionInnerDiameter/2, protrusionOuterDiameter/2, protrusionLength/2, 0, 2*M_PI);
+  auto protrusionLogicalVolume  = new G4LogicalVolume (protrusionSolid, aluminium, "CrystalHolderProtrusion");
+  auto protrusionPhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, protrusionShift),
+                                                     protrusionLogicalVolume,
+                                                     "CrystalHolderProtrusion",
+                                                     calorimeterLogicalVolume,
+                                                     false, 0, fCheckOverlaps);
+  
+  auto protrusionDiskSolid          = new G4Tubs ("CrystalHolderProtrusionDisk", 0, protrusionOuterDiameter/2 - protrusionThickness/2, protrusionThickness/2, 0, 2*M_PI);
+  auto protrusionDiskLogicalVolume  = new G4LogicalVolume (protrusionDiskSolid, aluminium, "CrystalHolderProtrusionDisk");
+  auto protrusionDiskPhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, protrusionDiskShift),
+                                                         protrusionDiskLogicalVolume,
+                                                         "CrystalHolderProtrusionDisk",
+                                                         calorimeterLogicalVolume,
+                                                         false, 0, fCheckOverlaps);
+  
+  auto protrusionTeflonSolid          = new G4Tubs ("CrystalHolderProtrusionTeflon", 0, teflonDiameter/2, teflonLength/2, 0, 2*M_PI);
+  auto protrusionTeflonLogicalVolume  = new G4LogicalVolume (protrusionTeflonSolid, teflon, "CrystalHolderProtrusionTeflon");
+  auto protrusionTeflonPhysicalVolume = new G4PVPlacement (0, G4ThreeVector(0, 0, teflonShift),
+                                                           protrusionTeflonLogicalVolume,
+                                                           "CrystalHolderProtrusionTeflon",
+                                                           calorimeterLogicalVolume,
+                                                           false, 0, fCheckOverlaps);
+  
+  //
   // Visualization attributes
   //
   worldLV->SetVisAttributes (G4VisAttributes::GetInvisible());
+  calorimeterLogicalVolume->SetVisAttributes(G4VisAttributes::GetInvisible());
   
-  auto simpleBoxVisAtt= new G4VisAttributes(G4Colour(1.0,1.0,1.0));
-  simpleBoxVisAtt->SetVisibility(true);
-  calorLV->SetVisAttributes(simpleBoxVisAtt);
-  
+  crystalLogicalVolume->SetVisAttributes (new G4VisAttributes(G4Color(1.0, 0.5, 0.5)));
   //
   // Always return the physical World
   //
